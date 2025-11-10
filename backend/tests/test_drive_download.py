f3d227aad6ab215b6bd32a97ef47ed56161bc081
Leoberto Bittencourt Filho <149162671+LeobertoB@users.noreply.github.com>
2025-11-10 20:50:33 +0100

    Delete backend/tests directory

--- backend/tests/test_drive_download.py
+++
@@ -1,8 +0,0 @@
-from drive_loader import download_missing_files, get_local_txt_files
-
-if __name__ == "__main__":
-    print("Initializing test")
-    download_missing_files()
-    print("\n Files downloaded: ")
-    for file in get_local_txt_files():
-        print(f"{file.name}")
